450da02fdcaa9a378f3b77912a98a4a9a70ca74a
Eniola011 <yettyagablu@gmail.com>
2023-07-19 16:20:56 -0700

    main.h: header file

---
+++ main.h
@@ -0,0 +1,45 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+/**
+ * struct builtins - handles builtin functions.
+ * @cmd: inputted commands
+ * @funt: function pointer
+ *
+ */
+struct builtins
+{
+	char *cmd;
+	void (*funt)(char **argmt);
+};
+
+char **get_token(char *line, ssize_t nread);
+char *read_a_line(ssize_t *nread);
+int exec_cmd(char **token);
+
+void builtin_command(char **arg);
+void shell_exit(char **arg);
+void shell_cd(char **arg);
+void shell_help(char **arg);
+void shell_env(char **arg);
+
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _strlen(const char *s);
+char *_strcat(char *dest, char *src);
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+char *_strdup(const char *s);
+
+char *find_path(char *cmd);
+
+#endif
