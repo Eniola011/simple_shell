c479384f46ef208fe255c175093c4c163f9f96aa
Eniola011 <yettyagablu@gmail.com>
2023-07-19 16:39:10 -0700

    memcpy

---
+++ 6memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies pointers
+ * @dest: pointer string
+ * @src: pointer string
+ * @n: size of pointer string
+ * Return: no value.
+ */
+void _memcpy(void *dest, const void *src, unsigned int n)
+{
+	char *cptr = (char *)src;
+	char *cptr1 = (char *)dest;
+	unsigned int index;
+
+	for (index = 0; index < size; index++)
+	{
+		cptr1[index] = cptr[index];
+	}
+}
