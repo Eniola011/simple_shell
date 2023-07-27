2926f9fd29d33b08ff5d485525d3bc7068b4436d
Eniola011 <yettyagbalu@gmail.com>
2023-07-27 13:27:50 -0700

    update:exit shell

--- exitsh.c
+++ exitsh.c
@@ -10,15 +10,15 @@ int exit_sh(list_sh *data)
 	unsigned int stats;
 	int digit;
 	int str_length;
-	int _num;
+	int num;
 
 	if (data->argc[1] != NULL)
 	{
 		stats = _atoi(data->argc[1]);
 		digit = _isdigit(data->argc[1]);
 		str_length = _strlen(data->argc[1]);
-		_num = stats > (unsigned int)INT_MAX;
-		if (!digit || str_length > 10 || _num)
+		num = stats > (unsigned int)INT_MAX;
+		if (!digit || str_length > 10 || num)
 		{
 			find_error(data, 2);
 			data->status = 2;
